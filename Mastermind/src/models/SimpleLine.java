88814d39d9eb7bd3fd86532e63d8bcda1e6f7030
Angus <pierresassusbourda@gmail.com>
2016-06-03 10:24:25 +0200

    SimpleLine Class

---
+++ Mastermind/src/models/SimpleLine.java
@@ -0,0 +1,27 @@
+package models;
+
+
+public class SimpleLine 
+{
+	
+	public SimpleLine() {
+		
+	}
+	
+public SimpleLine(Pawn pawn1, Pawn pawn2, Pawn pawn3, Pawn pawn4) {
+	
+	this.pawn1=pawn1;
+	this.pawn2=pawn2;
+	this.pawn3=pawn3;
+	this.pawn4= pawn4;
+		
+	}
+	
+	private Pawn pawn1;
+	private Pawn pawn2;
+	private Pawn pawn3;
+	private Pawn pawn4;
+	
+}
+
+
